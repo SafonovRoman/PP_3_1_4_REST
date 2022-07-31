0c6d1f7fc96e83c7e9b24d876b5d4e86749c71ee
Roman <safonov.roman.86@yandex.ru>
2022-07-31 17:48:31 +1000

    First try

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.model.Role;
 
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 @Service
